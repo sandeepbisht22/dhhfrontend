d2681de14b929b0e9e3f4ad60ef37aff75e02451
Sandeep <bishtsandy22@gmail.com>
2022-01-14 02:14:21 +0530

    test chnage Ip

--- src/state/actions/userAction.js
+++ src/state/actions/userAction.js
@@ -69,11 +69,7 @@ export const loginUser = (formData) => async (dispatch) => {
       },
     };
     //Will make a rest call to check if we can login and the set token returned
-    const res = await axios.post(
-      "https://desihiphop.herokuapp.com/auth",
-      formData,
-      config
-    );
+    const res = await axios.post("/auth", formData, config);
     console.log(
       "dispatching for LOGIN_USER_SUCCESS with res" + JSON.stringify(res.data)
     );
